5eed964493829f68cb5a7020adac24561392ab94
leepike <leepike@gmail.com>
2017-01-06 13:47:30 -0800

    wibbles to the POSIX examples.

--- tower-posix-tests/tests/Handlers.hs
+++ tower-posix-tests/tests/Handlers.hs
@@ -8,35 +8,35 @@ import Ivory.Tower
 import Ivory.OS.Posix.Tower
 import Ivory.OS.Posix.Tower.IO
 
-printf_2char :: Def ('[IString, Uint8, Uint8] ':-> ())
-printf_2char = importProc "printf" "stdio.h"
+printf_2 :: Def ('[IString, Uint8, Uint8] ':-> ())
+printf_2 = importProc "printf" "stdio.h"
 
 app :: Tower e ()
 app = do
   inChan <- readFD "stdin" stdin
-  dump <- period $ Milliseconds 100
+  dump   <- period $ Milliseconds 1000
 
   monitor "accumulate" $ do
     monitorModuleDef $ do
-      incl printf_2char
+      incl printf_2
 
-    minChar <- stateInit "minChar" $ ival maxBound
-    maxChar <- stateInit "maxChar" $ ival minBound
+    minVal <- stateInit "minChar" $ ival 109
+    maxVal <- stateInit "maxChar" $ ival 109
 
     handler inChan "reader" $ do
       callbackV $ \ ch -> do
-        lastMin <- deref minChar
-        when (ch <? lastMin) $ store minChar ch
-        lastMax <- deref maxChar
-        when (ch >? lastMax) $ store maxChar ch
+        lastMin <- deref minVal
+        -- Ignore ASCII 10 (return)
+        when (ch <? lastMin .&& ch /=? 10) $ store minVal ch
+        lastMax <- deref maxVal
+        -- Ignore ASCII 10 (return)
+        when (ch >? lastMax .&& ch /=? 10) $ store maxVal ch
 
     handler dump "dumper" $ do
       callback $ const $ do
-        lastMin <- deref minChar
-        lastMax <- deref maxChar
-        call_ printf_2char "range '%c' - '%c'\n" lastMin lastMax
-        store minChar maxBound
-        store maxChar minBound
+        lastMin <- deref minVal
+        lastMax <- deref maxVal
+        call_ printf_2 "range '%c' - '%c'\n" lastMin lastMax
 
 main :: IO ()
 main = compileTowerPosix (const $ return ()) app
--- tower-posix-tests/tests/Tick.hs
+++ tower-posix-tests/tests/Tick.hs
@@ -1,26 +1,42 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Ivory.Tower
+import Ivory.Stdlib
 import Ivory.Language
 import Ivory.OS.Posix.Tower
 
+printf :: Def ('[IString, Uint8] ':-> Sint32)
+printf = importProc "printf" "stdio.h"
+
 app :: Tower e ()
 app = do
+  towerModule appMod
+  towerDepends appMod
+
   (c1in, c1out) <- channel
   per <- period (Microseconds 1000)
   monitor "m1" $ do
     s <- state "local_st"
     handler per "tick" $ do
       e <- emitter c1in 1
-      callback $ \_ -> emit e (constRef (s :: Ref 'Global ('Stored Uint8)))
+      callback $ \_ -> do
+        s += 1
+        emit e (constRef (s :: Ref 'Global ('Stored Uint8)))
 
   monitor "m2" $ do
     s <- state "last_m2_chan1_message"
     handler c1out "chan1msg" $ do
-      callback $ \m ->
+      callback $ \m -> do
         refCopy s m
+        s' <- deref s
+        call_ printf "Val: %i\n" s'
+
+appMod :: Module
+appMod = package "appMod" $ incl printf
 
 main :: IO ()
 main = compileTowerPosix (const $ return ()) app
